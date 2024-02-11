cff6e7d06bc5d034d32c373cbd89e108aba23539
Iain Wood <mayasky76@gmail.com>
2024-02-11 17:39:06 +0000

    pwa manifest update

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,51 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
-
-// https://vitejs.dev/config/
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
-  base :'',
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  }
-})
+  base: "",
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate", // Automatically update the service worker
+      injectRegister: "auto", // Automatically inject the service worker registration
+      devOptions: { enabled: true },
+      manifest: {
+        "name": "Wavemaker Cards",
+        "short_name": "Wavemaker",
+        "theme_color": "#424242",
+        "icons": [
+            {
+                "src": "./img/icons/android-chrome-192x192.png",
+                "sizes": "192x192",
+                "type": "image/png"
+            },
+            {
+                "src": "./img/icons/android-chrome-512x512.png",
+                "sizes": "512x512",
+                "type": "image/png"
+            },
+            {
+                "src": "./img/icons/android-chrome-maskable-192x192.png",
+                "sizes": "192x192",
+                "type": "image/png",
+                "purpose": "maskable"
+            },
+            {
+                "src": "./img/icons/android-chrome-maskable-512x512.png",
+                "sizes": "512x512",
+                "type": "image/png",
+                "purpose": "maskable"
+            }
+        ],
+        "start_url": "./",
+        "display": "standalone",
+        "background_color": "#424242",
+        "scope": "./"       
+      },
+    }),
+  ],
+  resolve: { alias: { "@": fileURLToPath(new URL("./src", import.meta.url)) } },
+});
+
+ 
