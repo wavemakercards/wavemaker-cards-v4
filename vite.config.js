eecab17e86c077fc3026de6bbacaaae826ea5095
Iain Wood <iain.wood@open.ac.uk>
2025-10-22 11:55:43 +0100

    docs deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
   base: "",
   build: {
+    outDir: 'docs',
     rollupOptions: {
       output: {
         manualChunks: {
